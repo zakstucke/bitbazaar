560413b18f6d70084a25a4cc7419196596733b51
Zak Stucke <zakstucke@hotmail.co.uk>
2023-11-29 16:40:11 +0000

    Trying to fix build

--- js/tsup.config.ts
+++ js/tsup.config.ts
@@ -1,19 +1,21 @@
 import type { Options } from "tsup";
 
-const env = process.env.NODE_ENV;
-
 // https://dev.to/orabazu/how-to-bundle-a-tree-shakable-typescript-library-with-tsup-and-publish-with-npm-3c46
 export const tsup: Options = {
     splitting: true,
     clean: true, // clean up the dist folder
     dts: true, // generate dts files
     format: ["cjs", "esm"], // generate cjs and esm files
-    minify: env === "production",
-    bundle: env === "production",
+    minify: true,
+    bundle: false,
     skipNodeModulesBundle: true,
     entryPoints: ["bitbazaar/index.ts"],
-    watch: env === "development",
+    watch: false,
     target: "es2020",
-    outDir: "dist",
+    outDir: "./dist",
     entry: ["bitbazaar/**/*.ts"], // look at all files in the project
+    esbuildOptions(options, context) {
+        // the directory structure will be the same as the source
+        options.outbase = "./";
+    },
 };
