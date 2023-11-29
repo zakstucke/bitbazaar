9ea88283f3df7aa74fa3b2479f1fc02fbf4d02e1
Zak Stucke <zakstucke@hotmail.co.uk>
2023-11-29 17:02:49 +0000

    Might have fixed

--- js/tsup.config.ts
+++ js/tsup.config.ts
@@ -2,20 +2,13 @@ import type { Options } from "tsup";
 
 // https://dev.to/orabazu/how-to-bundle-a-tree-shakable-typescript-library-with-tsup-and-publish-with-npm-3c46
 export const tsup: Options = {
-    splitting: true,
     clean: true, // clean up the dist folder
     dts: true, // generate dts files
-    format: ["cjs", "esm"], // generate cjs and esm files
-    minify: true,
-    bundle: false,
+    format: ["esm"], // Only support ESM, fixes lots of import problems
+    minify: false, // Again allow downstream consumers to minify
+    bundle: false, // Don't bundle, allow downstream consumers to bundle
     skipNodeModulesBundle: true,
-    entryPoints: ["bitbazaar/index.ts"],
-    watch: false,
     target: "es2020",
     outDir: "./dist",
     entry: ["bitbazaar/**/*.ts"], // look at all files in the project
-    esbuildOptions(options, context) {
-        // the directory structure will be the same as the source
-        options.outbase = "./";
-    },
 };
